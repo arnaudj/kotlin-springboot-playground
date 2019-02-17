9d78236a0e59af30e6fadd501e0b8bfd2bf65a94
Arnaud J <arnaudj@users.noreply.github.com>
2019-02-17 19:23:16 +0100

    Initial commit from generator

---
+++ src/main/kotlin/com/arnaudj/sbplaygnd/sbplayground/SbplaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.arnaudj.sbplaygnd.sbplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbplaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbplaygroundApplication>(*args)
+}
